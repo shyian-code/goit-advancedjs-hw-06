dc09835d53aff42d146f7faf9531843fca99243f
Oleksandr Shyian <shyian.code@gmail.com>
2023-12-08 08:41:27 +0200

    basic types

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -1,6 +1,12 @@
-let some:unknown;
-some = 'Text';
+let some: unknown;
+some = "Text";
 let str: string;
-str = some;
+
+if (typeof some === "string") {
+  str = some;
+} else {
+  // Обробка випадку, коли тип не є string
+  console.error("Значення не є рядком");
+}
 
 export {};
--- src/generic/1.ts
+++ src/generic/1.ts
@@ -3,15 +3,13 @@
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
 
-function getPromise () {
-  return new Promise((resolve) => {
-    resolve(['Text', 50]);
+function getPromise<T>() {
+  return new Promise<T[]>((resolve) => {
+    resolve(['Text', 50] as T[]);
   });
 }
 
 getPromise()
-.then((data) => {
-  console.log(data);
-});
-
-export {};
+  .then((data) => {
+    console.log(data); // Тип data буде (string | number)[]
+  });
